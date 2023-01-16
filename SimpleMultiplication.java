fdb7af12f992aa82222db21a55e8e1001755be38
fizyomatik <59894817+fizyomatik@users.noreply.github.com>
2023-01-16 22:06:37 +0100

    Simple Math

---
+++ SimpleMultiplication.java
@@ -0,0 +1,15 @@
+
+
+import java.util.Scanner;
+
+public class SimpleMultiplication {
+
+    public static int simpleMultiplication(int i){
+      if (i%2==0){
+          return 8*i;
+      }else {
+          return 9*i;
+      }
+    }
+
+}
