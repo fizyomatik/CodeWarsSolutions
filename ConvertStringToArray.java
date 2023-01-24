eeef9b5f01f21cac5942bcffd1a321b084b01fc1
fizyomatik <59894817+fizyomatik@users.noreply.github.com>
2023-01-24 16:10:28 +0100

    String problem 8kyu

--- ConvertStringToArray.java
+++ ConvertStringToArray.java
@@ -1,33 +1,54 @@
 import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 import org.junit.runners.JUnit4;
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.runners.JUnit4;
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.runners.JUnit4;
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.runners.JUnit4;
+
 public class ConvertStringToArray {
-    public static void main(String[] args) {
-        String s="I love arrays they are my favorite";
-        System.out.println(stringToArray(s));
-    }
-    public static String[] stringToArray(String s){
+    public static String[] stringToArray(String s) {
 
-    int count=0;
-      for(int j=1;j<s.length();j++){
-        if(s.charAt(j)==' '){
-            count++;
+        int count = 0;
+        for (int j = 0; j < s.length(); j++) {
+            if (s.charAt(j) == ' ') {
+                count++;
+            }
         }
-    }
-
-    String [] st=new String[count+1];
-    int k=0,t=0;
-        for(int i=0;i<s.length();i++){
-
-        if(s.charAt(i)==' '){
-            st[k]=s.substring(t,i);
-            t=i+1;
-            k++;
-
+        if (count == 0) {
+            String[] stt = new String[1];
+            stt[0] = s.substring(0, s.length());
+            return stt;
         }
-    }
 
-      return st;
+        String[] st = new String[count + 1];
+        int k = 0, t = 0;
+
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == ' ') {
+                st[k] = s.substring(t, i);
+                t = i + 1;
+                k++;
+                if (k == (count)) {
+                    st[k] = s.substring(t, s.length());
+
+                }
+            }
+        }
+
+        return st;
+    }
+    @Test
+    public void basicTests() {
+        assertArrayEquals(new String[]{"Robin", "Singh"}, ConvertStringToArray.stringToArray("Robin Singh"));
+        assertArrayEquals(new String[]{"I", "love", "arrays", "they", "are", "my", "favorite"}, ConvertStringToArray.stringToArray("I love arrays they are my favorite"));
     }
 
 }
